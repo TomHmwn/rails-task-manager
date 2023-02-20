501848feec06e40444e79546a8a1691a9b66995e
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-02-20 14:39:53 +1100

    add checkboxes, crud operations and refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,33 +13,35 @@ class TasksController < ApplicationController
   end
 
   def create
+    # raise
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path
+      redirect_to task_path(@task)
     else
       render :new
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_path
+    redirect_to tasks_path, status: :see_other
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details, :completed)
+    params.require(:task).permit(:title, :details, :completed, :oppetool)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
